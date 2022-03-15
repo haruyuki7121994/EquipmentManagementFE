6b3f2ea6e04d102735f178346a159d644d54eaae
Haruyuki <haruyuki7121994@gmail.com>
2022-03-15 17:34:55 +0700

    update ui

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -8,11 +8,15 @@ export const authReducer = createSlice({
         'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbjEiLCJpYXQiOjE2NDY4MTgzMDAsImV4cCI6MTY0NjkwNDcwMH0.iPAoT75PfGMLZ_aFLhfFpMppGk6JCAedC4ok3RcEM6A8MNOB1aJbtOGaaIkRytgCAPnKU4wH4j8fpsj30jonyg',
     },
     isLogin: false,
+    isLogout: false,
   },
   reducers: {
     setAuth: (state, action) => {
       state.isLogin = action.payload.isLogin
       state.data = action.payload.data
+    },
+    setLogout: (state, action) => {
+      state.isLogout = action.payload.isLogout
     },
   },
 })
--- src/views/pages/logout/Logout.js
+++ src/views/pages/logout/Logout.js
@@ -1,30 +1,17 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { authReducer } from '../../../redux/reducers/authReducer'
 import CookieService from '../../../services/CookieService'
 import { useHistory } from 'react-router-dom'
 
 const Logout = () => {
-  const dispatch = useDispatch()
   const history = useHistory()
-  const alertVisible = useSelector((state) => state.alert.visible)
-  const handleLogout = () => {
-    dispatch(
-      authReducer.actions.setAuth({
-        isLogin: false,
-        data: { token: '' },
-      }),
-    )
-    CookieService.remove('access_token')
-    console.log('logout')
+  if (CookieService.get('access_token') === undefined) {
     history.push('/login')
   }
-  if (alertVisible) {
-    setTimeout(() => {
-      handleLogout()
-    }, 3000)
-  } else {
-    handleLogout()
+  const handleLogout = () => {
+    CookieService.remove('access_token')
+    console.log('logout')
+    document.location.reload()
   }
+  handleLogout()
   return null
 }
 export default Logout
